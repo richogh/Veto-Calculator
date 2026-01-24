1d2a9e2b07e7b925087cd699922ed88d2417f9d3
richogh <richonew261018@gmail.com>
2026-01-24 16:23:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,62 +1,70 @@
 // {}
 
-const UKUR_HARGA = {
+// };
+
+const UH = {
     "25 x 35": 18500,
     "30 x 40": 22500,
     "38 x 45": 27600
 };
-const keys = Object.keys(UKUR_HARGA);
-
-const select = document.getElementById("operator");
-keys.forEach(v => select.add(new Option(v, v)));
-
-
-/// SIMPLE ///
-const Afdruk = 5000;
-// Emulsi Sepaket, Byclean, LakBan, M3/M4
-const Tinta = 700;
-// Rubber, Printol, Binder, Penanda
-const Lain2 = 300;
-// Kassa, M3, Botol, Stick Eskrim, dll
-
-const Final = Afdruk+Tinta+Lain2;
-const ProfitLusin = 13000;
-const ProfitFlexible = {
-    8: ProfitLusin-2000,
-    25: ProfitLusin-3500,
-    41: ProfitLusin-5000,
-    83: ProfitLusin-6000
+/////1352 -- 2300
+const PLusin = 13000;
+const PFlex = {
+    8: PLusin-2000,
+    25: PLusin-3500,
+    41: PLusin-5000,
+    83: PLusin-6000
 
 
 };
-const poinlusin = Object.keys(ProfitFlexible).map(Number);
+/////
+const [Rubber, Printol, Binder] = [15000, 30000, 40000];
+const Afdruk = 5000;
+const Missing = 300;
 
-// Math.ceil(harga_jual_a*quantity)
+function tintaaa() {
+    pr_rb = (Printol/1000)*40;
+    bi_rb = (Binder/1000)*40;
 
+    return (Rubber+pr_rb+bi_rb)/125
+};
+function open_wa() {
+    window.open("https://wa.me/62895388390106?text=Halo..%0ASaya%20mau%20bertanya%20soal%20Tas%20Spunbond")
+}
+
+
+
+///////////////////////////////////////////////////
+
+const keys = Object.keys(UH);
+const select = document.getElementById("operator");
+keys.forEach(v => select.add(new Option(v, v)));
+
+const poinlusin = Object.keys(PFlex).map(Number);
 function SPUNBOND(quantity, key_ukur) {
-    let all_modal = ((UKUR_HARGA[key_ukur]/12)*quantity)+Final;
+    let all_modal = ((UH[key_ukur]/12)*quantity)+Afdruk+Missing+(tintaaa()*quantity);
     
-    let all_profit_base_quan = (ProfitLusin/12)*quantity;
+    let all_profit_base_quan = (PLusin/12)*quantity;
     const total_bagi_lusin = Math.ceil(quantity/12);
-    let cek;
     if (total_bagi_lusin >= 8) {
         for (let i of poinlusin) {
             if (total_bagi_lusin >= i) {
-                all_profit_base_quan = (ProfitFlexible[i]/12)*quantity;
-                cek = ProfitFlexible[i]
+                all_profit_base_quan = (PFlex[i]/12)*quantity;
             } else {break}
         };
     };
 
-    let harga_jual_a;
-    if (all_profit_base_quan <= ProfitLusin) {
-        harga_jual_a = (all_modal+ProfitLusin)/quantity;
+    let price_item;
+    if (all_profit_base_quan <= PLusin) {
+        price_item = (all_modal+PLusin)/quantity;
     }else {
-        harga_jual_a = (all_modal+all_profit_base_quan)/quantity;
+        price_item = (all_modal+all_profit_base_quan)/quantity;
     };
 
-    return [Math.ceil(harga_jual_a), Math.ceil(harga_jual_a*quantity)];
+    return [Math.ceil(price_item), Math.ceil(price_item)*quantity];
 }
+
+///////////////////////////////////////////////////
 
 function hitung() {
     let a = Number(document.getElementById("angka1").value);
@@ -67,6 +75,3 @@ function hitung() {
     document.getElementById("total").innerText = `Rp. ${Total_bayar.toLocaleString("id-ID")}`;
 };
 
-function open_wa() {
-    window.open("https://wa.me/62895388390106?text=Halo..%0ASaya%20mau%20bertanya%20soal%20Tas%20Spunbond")
-}
